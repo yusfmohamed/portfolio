1a47b40306f2147c05f19a2c389cdfd0215f9ce6
Youssef Mohamed <yucifmohamed@gmail.com>
2026-04-13 23:01:03 +0200

    new

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// Company card click (placeholder)
-document.querySelectorAll('.portfolio-card').forEach(card => {
-  card.addEventListener('click', () => {
-    alert('This will open your work for this company.');
-  });
-});
